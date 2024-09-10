24a071d7989e39bc19bf6baec99c66ed1cd27ccd
Muralidhar Basani <muralidhar.basani@aiven.io>
2024-09-10 10:38:13 +0200

    EC-243 Rename package

--- s3-source-connector/src/integration-test/java/io/aiven/kafka/connect/s3/source/IntegrationTest.java
+++ s3-source-connector/src/integration-test/java/io/aiven/kafka/connect/s3/source/IntegrationTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.aiven.kafka.connect.source.s3;
+package io.aiven.kafka.connect.s3.source;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
