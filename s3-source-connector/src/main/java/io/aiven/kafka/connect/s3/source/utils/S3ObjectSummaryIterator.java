bda8854eb700c634b36799bfb658b3e3546f16fc
¨Claude <claude.warren@aiven.io>
2024-11-12 14:38:13 +0000

    fixed spotless issues

--- s3-source-connector/src/main/java/io/aiven/kafka/connect/s3/source/utils/S3ObjectSummaryIterator.java
+++ s3-source-connector/src/main/java/io/aiven/kafka/connect/s3/source/utils/S3ObjectSummaryIterator.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.aiven.kafka.connect.s3.source.utils;
 
 import java.util.Iterator;
