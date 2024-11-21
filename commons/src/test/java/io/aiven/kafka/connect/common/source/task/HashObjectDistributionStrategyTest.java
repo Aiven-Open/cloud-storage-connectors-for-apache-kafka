2bb047f5fef54757c70e6a29ff9006dd82aa7c23
Aindriú Lavelle <121855584+aindriu-aiven@users.noreply.github.com>
2024-11-21 08:38:13 +0000

    Common source task assignment (#345)
    Signed-off-by: Aindriu Lavelle <aindriu.lavelle@aiven.io>

---
+++ commons/src/test/java/io/aiven/kafka/connect/common/source/task/HashObjectDistributionStrategyTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.connect.common.source.task;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+final class HashObjectDistributionStrategyTest {
+
+    @ParameterizedTest
+    @CsvSource({ "logs-0-0002.txt", "logs-1-0002.txt", "logs-2-0002.txt", "logs-3-0002.txt", "logs-4-0002.txt",
+            "logs-5-0002.txt", "logs-6-0002.txt", "logs-7-0002.txt", "logs-8-0002.txt", "logs-9-0002.txt", "key-0.txt",
+            "logs-1-0002.txt", "key-0002.txt", "logs-3-0002.txt", "key-0002.txt", "logs-5-0002.txt", "value-6-0002.txt",
+            "logs-7-0002.txt", "anImage8-0002.png",
+            "reallylongfilenamecreatedonS3tohisdesomedata and alsohassome spaces.txt" })
+    void hashDistributionExactlyOnce(final String path) {
+        final int maxTaskId = 10;
+        final ObjectDistributionStrategy taskDistribution = new HashObjectDistributionStrategy(maxTaskId);
+        final List<Boolean> results = new ArrayList<>();
+        for (int taskId = 0; taskId < maxTaskId; taskId++) {
+            results.add(taskDistribution.isPartOfTask(taskId, path));
+        }
+        assertThat(results).containsExactlyInAnyOrder(Boolean.TRUE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE,
+                Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "logs-0-0002.txt", "logs-1-0002.txt", "logs-2-0002.txt", "logs-3-0002.txt", "logs-4-0002.txt",
+            "logs-5-0002.txt", "logs-6-0002.txt", "logs-7-0002.txt", "logs-8-0002.txt", "logs-9-0002.txt", "key-0.txt",
+            "logs-1-0002.txt", "key-0002.txt", "logs-3-0002.txt", "key-0002.txt", "logs-5-0002.txt", "value-6-0002.txt",
+            "logs-7-0002.txt", "anImage8-0002.png",
+            "reallylongfilenamecreatedonS3tohisdesomedata and alsohassome spaces.txt" })
+    void hashDistributionExactlyOnceWithReconfigureEvent(final String path) {
+        int maxTasks = 10;
+        final ObjectDistributionStrategy taskDistribution = new HashObjectDistributionStrategy(maxTasks);
+        final List<Boolean> results = new ArrayList<>();
+        for (int taskId = 0; taskId < maxTasks; taskId++) {
+            results.add(taskDistribution.isPartOfTask(taskId, path));
+        }
+        assertThat(results).containsExactlyInAnyOrder(Boolean.TRUE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE,
+                Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE);
+        results.clear();
+        maxTasks = 5;
+        taskDistribution.reconfigureDistributionStrategy(maxTasks, null);
+        for (int taskId = 0; taskId < maxTasks; taskId++) {
+            results.add(taskDistribution.isPartOfTask(taskId, path));
+        }
+        assertThat(results).containsExactlyInAnyOrder(Boolean.TRUE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE,
+                Boolean.FALSE);
+    }
+}
--- s3-sink-connector/README.md
+++ s3-sink-connector/README.md
@@ -238,7 +238,7 @@ Connector class name, in this case: `io.aiven.kafka.connect.s3.AivenKafkaConnect
 ### S3 Object Names
 
 S3 connector stores series of files in the specified bucket.
-Each object is named using pattern `[<aws.s3.prefix>]<topic>-<partition>-<startoffset>[.gz]` (see [#file-name-format](File name format section) for more patterns).
+Each object is named using pattern `[<aws.s3.prefix>]<topic>-<partition>-<start_offset>[.gz]` (see [#file-name-format](File name format section) for more patterns).
 The `.gz` extension is used if gzip compression is used, see `file.compression.type` below.
 
 ### Data File Format
