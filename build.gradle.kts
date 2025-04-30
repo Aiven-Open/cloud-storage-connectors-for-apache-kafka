85a834c322fde16c380e06d91c1f7c0e350d2034
¨Claude <claude.warren@aiven.io>
2025-04-30 16:04:52 +0100

    fixed checkstyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,31 @@
-plugins {
-    id("aiven-apache-kafka-connectors-all.java-conventions")
-}
-
+plugins { id("aiven-apache-kafka-connectors-all.java-conventions") }
 
 copy {
-    println("Copying from ${rootProject.layout.projectDirectory.asFile}/*.md")
-    println("          to ${project.layout.buildDirectory.asFile.get()}/site/")
-    from("${rootProject.layout.projectDirectory}") {
-        include("*.md")
-        rename { fileName ->
-            // a simple way is to remove the "-$version" from the jar filename
-            // but you can customize the filename replacement rule as you wish.
-            fileName.replace("README", "index")
-        }
+  println("Copying from ${rootProject.layout.projectDirectory.asFile}/*.md")
+  println("          to ${project.layout.buildDirectory.asFile.get()}/site/")
+  from("${rootProject.layout.projectDirectory}") {
+    include("*.md")
+    rename { fileName ->
+      // a simple way is to remove the "-$version" from the jar filename
+      // but you can customize the filename replacement rule as you wish.
+      fileName.replace("README", "index")
     }
-    into("${project.layout.buildDirectory.asFile.get()}/site/markdown/")
+  }
+  into("${project.layout.buildDirectory.asFile.get()}/site/markdown/")
 }
 
 tasks.register<Copy>("copySiteAssets") {
-    outputs.upToDateWhen { false }
-    println("Copying from ${rootProject.layout.projectDirectory.asFile}/*.md")
-    println("          to ${project.layout.projectDirectory.asFile}/site/build/site/markdown")
-    from("${rootProject.layout.projectDirectory}") {
-        include("*.md")
+  outputs.upToDateWhen { false }
+  println("Copying from ${rootProject.layout.projectDirectory.asFile}/*.md")
+  println("          to ${project.layout.projectDirectory.asFile}/site/build/site/markdown")
+  from("${rootProject.layout.projectDirectory}") { include("*.md") }
+  from("${rootProject.layout.projectDirectory}") {
+    include("README*.md")
+    rename { fileName ->
+      // a simple way is to remove the "-$version" from the jar filename
+      // but you can customize the filename replacement rule as you wish.
+      fileName.replace("README", "index")
     }
-    from("${rootProject.layout.projectDirectory}") {
-        include("README*.md")
-        rename { fileName ->
-            // a simple way is to remove the "-$version" from the jar filename
-            // but you can customize the filename replacement rule as you wish.
-            fileName.replace("README", "index")
-        }
-    }
-    into("${project.layout.projectDirectory.asFile}/site/build/site/markdown")
+  }
+  into("${project.layout.projectDirectory.asFile}/site/build/site/markdown")
 }
