84cfa8a9da74a9fcda5e1271833a0b488e1f6828
Davide Armand <davide.armand@aiven.io>
2025-05-26 11:51:43 +0200

    Implement reading blobs from Azure for source connector

--- azure-source-connector/src/main/java/io/aiven/kafka/connect/azure/source/AzureBlobSourceTask.java
+++ azure-source-connector/src/main/java/io/aiven/kafka/connect/azure/source/AzureBlobSourceTask.java
@@ -16,23 +16,34 @@
 
 package io.aiven.kafka.connect.azure.source;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 
 import org.apache.kafka.connect.source.SourceRecord;
 
 import io.aiven.kafka.connect.azure.source.config.AzureBlobSourceConfig;
-import io.aiven.kafka.connect.azure.source.utils.VersionInfo;
+import io.aiven.kafka.connect.azure.source.utils.AzureBlobClient;
+import io.aiven.kafka.connect.azure.source.utils.AzureBlobOffsetManagerEntry;
+import io.aiven.kafka.connect.azure.source.utils.AzureBlobSourceRecord;
+import io.aiven.kafka.connect.azure.source.utils.AzureBlobSourceRecordIterator;
 import io.aiven.kafka.connect.common.config.SourceCommonConfig;
 import io.aiven.kafka.connect.common.source.AbstractSourceTask;
+import io.aiven.kafka.connect.common.source.OffsetManager;
+import io.aiven.kafka.connect.common.utils.VersionInfo;
 
+import org.apache.commons.collections4.IteratorUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AzureBlobSourceTask extends AbstractSourceTask {
     /* The logger to write to */
     private static final Logger LOGGER = LoggerFactory.getLogger(AzureBlobSourceTask.class);
+
+    private OffsetManager<AzureBlobOffsetManagerEntry> offsetManager;
+    /** The configuration for this run */
+    private AzureBlobSourceConfig azureBlobSourceConfig; // NOPMD only called once, when used in the future this can be
+    // removed
 
     /**
      * Constructor to set the Logger used. This constructor is required by Connect.
@@ -41,23 +52,38 @@ public class AzureBlobSourceTask extends AbstractSourceTask {
     public AzureBlobSourceTask() {
         super(LOGGER);
     }
-    private AzureBlobSourceConfig azureBlobSourceConfig; // NOPMD only called once, when used in the future this can be
-                                                         // removed
+
+    /**
+     * For testing access.
+     *
+     * @param iterator
+     *            the iterator to read.
+     */
+    protected AzureBlobSourceTask(final Iterator<AzureBlobSourceRecord> iterator) {
+        this();
+        azureBlobSourceRecordIterator = iterator;
+    }
 
     @Override
     protected Iterator<SourceRecord> getIterator(final BackoffConfig config) {
-        return Collections.emptyIterator();
+        final Iterator<SourceRecord> inner = IteratorUtils.transformedIterator(azureBlobSourceRecordIterator,
+                r -> r.getSourceRecord(azureBlobSourceConfig.getErrorsTolerance(), offsetManager));
+        return IteratorUtils.filteredIterator(inner, Objects::nonNull);
     }
 
     @Override
     protected SourceCommonConfig configure(final Map<String, String> props) {
         this.azureBlobSourceConfig = new AzureBlobSourceConfig(props);
+        offsetManager = new OffsetManager<>(context);
+        final AzureBlobClient azureBlobClient = new AzureBlobClient(azureBlobSourceConfig);
+        azureBlobSourceRecordIterator = new AzureBlobSourceRecordIterator(azureBlobSourceConfig, offsetManager,
+                azureBlobSourceConfig.getTransformer(), azureBlobClient);
         return azureBlobSourceConfig;
     }
 
     @Override
     protected void closeResources() {
-
+        // nothing to do.
     }
 
     @Override
