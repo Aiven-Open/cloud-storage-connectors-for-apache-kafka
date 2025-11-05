eac46f3fd0f787f7a58b59704c1a7ac7f96840db
Claude Warren <claude.warren@aiven.io>
2025-11-05 09:05:04 +0000

    cleaned up dependency

--- azure-source-connector/build.gradle.kts
+++ azure-source-connector/build.gradle.kts
@@ -59,8 +59,8 @@ dependencies {
   compileOnly("org.apache.velocity:velocity-engine-core:2.4.1")
   compileOnly("org.apache.velocity.tools:velocity-tools-generic:3.1")
 
-  implementation("commons-io:commons-io:2.18.0")
-  implementation("org.apache.commons:commons-lang3:3.17.0")
+  implementation(apache.commons.io)
+  implementation(apache.commons.lang3)
   implementation(project(":commons"))
   implementation(apache.commons.collection4)
   implementation("com.azure:azure-storage-blob:12.30.0")
